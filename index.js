774a99ec8ec9e8b9ed3339bb3ed3d8b0d7976385
otaroo <otaroo@hotmail.co.th>
2017-09-21 16:31:16 +0700

    edot

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var app = express();
+const line = require('@line/bot-sdk');
+
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+const client = new line.Client(config);
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,7 +18,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  //response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
