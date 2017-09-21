cac32bbf6fbcc4736399c63e7f322f8fc8f52855
otaroo <otaroo@hotmail.co.th>
2017-09-21 17:21:17 +0700

    edit

--- index.js
+++ index.js
@@ -13,29 +13,8 @@ const client = new line.Client(config);
 app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
-// app.post('/webhook', line.middleware(config), (req, res) => {
-//   Promise
-//     .all(req.body.events.map(handleEvent))
-//     .then((result) => res.json(result));
-// });
-// function handleEvent(event) {
-//   if (event.type !== 'message' || event.message.type !== 'text') {
-//     return Promise.resolve(null);
-//   }
-//   const echo = { type: 'text', text: event.message.text };
-//   return client.replyMessage(event.replyToken, echo);
-// }
-app.post('/webhook', (req, res) => {
-  // var text = req.body.events[0].message.text
-  // var sender = req.body.events[0].source.userId
-  // var replyToken = req.body.events[0].replyToken
-  // console.log(text, sender, replyToken)
-  // console.log(typeof sender, typeof text)
-  // // console.log(req.body.events[0])
-  // if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
-  //   sendText(sender, text)
-  // }
-  // res.sendStatus(200)
+
+app.post('/webhook', (req, res) => {  
   Promise
   .all(req.body.events.map(handleEvent))
   .then((result) => res.json(result));
@@ -49,38 +28,19 @@ function handleEvent(event) {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
+    if(event.message.text === "[]"){
+    // create a echoing text message
+    const echo = { type: 'text', text: event.message.text };
+    
+      // use reply API
+      return client.replyMessage(event.replyToken, echo);
 
-  // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
+    }else{
+      return Promise.resolve(null);
+    }
+ 
+}
 
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
-}
-function sendText (sender, text) {
-  let data = {
-    to: sender,
-    messages: [
-      {
-        type: 'text',
-        text: 'สวัสดีค่ะ เราเป็นผู้ช่วยปรึกษาด้านความรัก สำหรับหมามิ้น 💞'
-      }
-    ]
-  }
-  request({
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer Yfp4E1/cS+OUoQOVVHc2/uLctihQ5gHv9o5rPRMLp0drPl0ObyZwI8uYQjm/VozeGloTmKsOnpdNdwmUrJTw91JQX3LJG3bVSpRFe/q++N0p0ZuTsLoksNRK6TBkmR4+KIgNplG7sib3btmH6nYuowdB04t89/1O/w1cDnyilFU='
-    },
-    url: 'https://api.line.me/v2/bot/message/push',
-    method: 'POST',
-    body: data,
-    json: true
-  }, function (err, res, body) {
-    if (err) console.log('error')
-    if (res) console.log('success')
-    if (body) console.log(body)
-  })
-}
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
